a93713bdf5c565fd96ca005ec46624df4d74d197
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-03-14 10:54:43 +0000

    classifier: pretrained: Change last layer of pretrained model
    Change for desired classes number.
    
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/classifier/pretrained.py
+++ src/classifier/pretrained.py
@@ -7,14 +7,11 @@ class ClassifierVIT(nn.Module):
     def __init__(self, img_size, num_classes, nf, device):
         super(ClassifierVIT, self).__init__()
         num_channels = img_size[0]
+        out_features = 1 if num_classes == 2 else num_classes
         self.device = device
-        self.model = model = AutoModelForImageClassification.from_pretrained("farleyknight-org-username/vit-base-mnist")
-        self.dropout = nn.Dropout(0.10)
-        self.predictor = nn.Sequential(
-            nn.Linear(10, 1 if num_classes ==
-                      2 else num_classes),
-            nn.Sigmoid() if num_classes == 2 else nn.Softmax(dim=1)
-        )
+        self.model = AutoModelForImageClassification.from_pretrained("farleyknight-org-username/vit-base-mnist")
+        self.model.classifier = nn.Linear(in_features=self.model.classifier.in_features, out_features=out_features, bias=True)
+        self.model.num_labels = out_features
         self.transforms = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Lambda(lambda pil_img: pil_img.convert("RGB")),
@@ -24,34 +21,37 @@ class ClassifierVIT(nn.Module):
 
     def forward(self, x, output_feature_maps=False):
         output = torch.Tensor().to(self.device)
-        for i in x:
-            o = self.model(self.transforms(i).unsqueeze(0).to(self.device))
-            out = self.dropout(o['logits']) # Pick up logits
-            output = torch.cat((output, out), dim=0)
-
-        if output_feature_maps:
-            return output
-        else:
-            return self.predictor(output)
+        images = torch.stack([self.transforms(i) for i in x]).to(self.device)
+        return self.model(images)['logits']
 
 class Ensemble(nn.Module):
     def __init__(self, img_size, num_classes, nf, device):
         super(Ensemble, self).__init__()
         num_channels = img_size[0]
         self.device = device
+        self.model_1 = ClassifierVIT(img_size, num_classes, nf, self.device).to(self.device)
         self.models = [
-            ClassifierVIT(img_size, num_classes, nf, self.device).to(self.device),
+            self.model_1,
         ]
         # TO DISCUSS
         self.predictor = nn.Sequential(
             nn.Linear(len(self.models), 1),
+            nn.Sigmoid(),
         ).to(self.device)
         # TO DISCUSS
 
     def forward(self, x, output_feature_maps=False):
         output = torch.Tensor().to(self.device)
+        feat_maps = []
         for m in self.models:
             out = m(x.to(self.device), output_feature_maps)
             output = torch.cat((output, out), dim=1)
 
-        return self.predictor(output).squeeze(1)
+        feat_maps.append(output)
+        output = self.predictor(output).squeeze(1)
+        feat_maps.append(output)
+
+        if output_feature_maps:
+            return feat_maps
+        else:
+            return output
