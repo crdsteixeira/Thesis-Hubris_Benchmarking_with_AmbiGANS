348836a032f492236618b5979848bb6470f502ba
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-03-18 07:46:17 +0000

    classifier: pretrained: Fixed custom layer
    Fixed the custom layer logic for training.
    Added normalizing transformation required for ViT.
    Froze pre-trained layers.
    
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/classifier/pretrained.py
+++ src/classifier/pretrained.py
@@ -10,18 +10,22 @@ class ClassifierVIT(nn.Module):
         out_features = 1 if num_classes == 2 else num_classes
         self.device = device
         self.model = AutoModelForImageClassification.from_pretrained("farleyknight-org-username/vit-base-mnist")
-        self.model.classifier = nn.Linear(in_features=self.model.classifier.in_features, out_features=out_features, bias=True)
+
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        self.model.classifier = nn.Linear(in_features=self.model.classifier.in_features, out_features=out_features)
         self.model.num_labels = out_features
         self.transforms = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Lambda(lambda pil_img: pil_img.convert("RGB")),
             transforms.Resize(224),
             transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
 
     def forward(self, x, output_feature_maps=False):
-        output = torch.Tensor().to(self.device)
-        images = torch.stack([self.transforms(i) for i in x]).to(self.device)
+        images = torch.stack([self.transforms(i).to(self.device) for i in x])
         return self.model(images)['logits']
 
 class Ensemble(nn.Module):
@@ -29,7 +33,7 @@ class Ensemble(nn.Module):
         super(Ensemble, self).__init__()
         num_channels = img_size[0]
         self.device = device
-        self.model_1 = ClassifierVIT(img_size, num_classes, nf, self.device).to(self.device)
+        self.model_1 = ClassifierVIT(img_size, num_classes, nf, self.device)
         self.models = [
             self.model_1,
         ]
@@ -37,14 +41,14 @@ class Ensemble(nn.Module):
         self.predictor = nn.Sequential(
             nn.Linear(len(self.models), 1),
             nn.Sigmoid(),
-        ).to(self.device)
+        )
         # TO DISCUSS
 
     def forward(self, x, output_feature_maps=False):
         output = torch.Tensor().to(self.device)
         feat_maps = []
         for m in self.models:
-            out = m(x.to(self.device), output_feature_maps)
+            out = m(x, output_feature_maps)
             output = torch.cat((output, out), dim=1)
 
         feat_maps.append(output)
