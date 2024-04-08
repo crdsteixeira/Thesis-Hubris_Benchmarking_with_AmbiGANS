d341d006e8da71024dbb5ff9c70a93e2b22f8fd5
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-04-08 21:28:36 +0000

    classifier: pretrained: Added MLP to ensemble
    Added pre-trained MLP model to list of ensemble models.
    Deactivated ViT due to CUDA memory restrictions.
    
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/classifier/pretrained.py
+++ src/classifier/pretrained.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from transformers import AutoImageProcessor, AutoModelForImageClassification
+from transformers import AutoImageProcessor, AutoModelForImageClassification, AutoModel
 from torchvision import transforms
 
 class ClassifierVIT(nn.Module):
@@ -48,16 +48,41 @@ class ClassifierResnet(nn.Module):
     def forward(self, x, output_feature_maps=False):
         return self.model(x)['logits']
 
+class ClassifierMLP(nn.Module):
+    def __init__(self, img_size, num_classes, nf, device):
+        super(ClassifierMLP, self).__init__()
+        num_channels = img_size[0]
+        out_features = 1 if num_classes == 2 else num_classes
+        self.device = device
+        self.model = AutoModel.from_pretrained("dacorvo/mnist-mlp", trust_remote_code=True)
+
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        self.model.output_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=self.model.config.hidden_size, out_features=out_features),
+        )
+        self.transforms = transforms.Compose([
+            transforms.Normalize((0.1307), (0.3081)),
+            transforms.Lambda(lambda x: torch.flatten(x)),
+        ])
+
+    def forward(self, x, output_feature_maps=False):
+        images = torch.stack([self.transforms(i).to(self.device) for i in x])
+        return self.model(images)
+
 class Ensemble(nn.Module):
     def __init__(self, img_size, num_classes, nf, device):
         super(Ensemble, self).__init__()
         num_channels = img_size[0]
         self.device = device
-        self.model_1 = ClassifierVIT(img_size, num_classes, nf, self.device)
+        #self.model_1 = ClassifierVIT(img_size, num_classes, nf, self.device)
         self.model_2 = ClassifierResnet(img_size, num_classes, nf, self.device)
+        self.model_3 = ClassifierMLP(img_size, num_classes, nf, self.device)
         self.models = [
-            self.model_1,
             self.model_2,
+            self.model_3,
         ]
         # TO DISCUSS
         self.predictor = nn.Sequential(
